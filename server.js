7c515c7160811763c3c2121e1d5fb6f90d10716f
Allen Clemons <allenclemons92@gmail.com>
2019-09-02 00:16:03 -0400

    Found another typo

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(forceSSL());
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
 })
 // Start the app by listening on the default
 // Heroku port
