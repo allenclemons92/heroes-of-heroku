b33e4c67cb0fcec5de9e9bbfe09f61ea7a2b5c95
Allen Clemons <allenclemons92@gmail.com>
2019-09-02 00:54:48 -0400

    So sleepy

--- server.js
+++ server.js
@@ -25,12 +25,8 @@ app.use(forceSSL());
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/angular-tour-of-heroes/index.html'));
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
 })
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080, () => {
-    console.log(path.join(__dirname, '/dist/angular-tour-of-heroes/index.html'));
-    console.log('Listening...');
-    console.log(process.env.PORT);
-});
+app.listen(process.env.PORT || 8080, () => {});
